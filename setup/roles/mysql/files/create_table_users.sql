cf1b719225d7c9e8873b325da60e96f263e16293
smrmkt <smrmkt@gmail.com>
2016-10-05 16:03:16 +0900

    デフォルトユーザの追加

--- setup/roles/mysql/files/create_table_users.sql
+++ setup/roles/mysql/files/create_table_users.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `authority` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `users` (`username`, `password`, `authority`) VALUES (
+  'root',
+  '7dc46644b58a5aeb755281cd77e64e417daa2659',
+  'administrator'
+);
+
